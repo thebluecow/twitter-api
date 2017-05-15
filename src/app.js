b8935cbcf97fb2edfe1f4041d315b141afea4e23
Scott Bline <thebluecow@msn.com>
2017-05-15 13:03:11 -0400

    updated directory structure and built index.pug for tweet info

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-let express = require('express');
+const express = require('express');
+const path = require('path');
 let app = express();
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
 	// same as { path: path }
 	let path = req.path;
 	res.locals.path = path;
-	res.render('index');
+	res.render('index', { username: "thebluecow1" } );
 });
 
 app.listen(3000, function() {
